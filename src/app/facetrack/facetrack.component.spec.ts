f0e27b0605bcec1077642cdfe49112718cdebe50
Ignacio Buioli <ibuioli@gmail.com>
2021-01-20 18:02:14 -0300

    feat: migrate ng11-cli

--- src/app/facetrack/facetrack.component.spec.ts
+++ src/app/facetrack/facetrack.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FacetrackComponent } from './facetrack.component';
 
@@ -6,7 +6,7 @@ describe('FacetrackComponent', () => {
   let component: FacetrackComponent;
   let fixture: ComponentFixture<FacetrackComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FacetrackComponent ]
     })
--- src/app/facetrack/three/three.component.spec.ts
+++ src/app/facetrack/three/three.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ThreeComponent } from './three.component';
 
@@ -6,7 +6,7 @@ describe('ThreeComponent', () => {
   let component: ThreeComponent;
   let fixture: ComponentFixture<ThreeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ThreeComponent ]
     })
